73e6efbbf3dfaaef4f4d05914a06dfc6a1b7535b
JuanDT <juandavidtoromesa2@gmail.com>
2023-03-25 21:06:48 -0500

    crud_empleados con diseño terminado

--- crud_empleados/src/main/java/com/crud/crud_empleados/entities/Empleado.java
+++ crud_empleados/src/main/java/com/crud/crud_empleados/entities/Empleado.java
@@ -1,5 +1,8 @@
 package com.crud.crud_empleados.entities;
 
+import org.springframework.validation.annotation.Validated;
+
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,7 +12,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 
+@Validated
 @Entity
 @Table(name = "empleado")
 public class Empleado {
@@ -27,7 +32,8 @@ public class Empleado {
 
     @Column(name = "apellido2")
     private String apellido2;
-
+    
+    @Email(message = "El email debe ser válido")
     @Column(name = "email")
     private String email;
 
